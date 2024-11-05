9d1ce7da4180d93af9060811a31d5046fd304ebb
fancyyyff <f16fy@outlook.com>
2024-11-05 16:03:59 +0800

    重新理顺了各部分的联系，确定了各部分的主要功能、侧边栏的布局，只是暂时只完成了侧边栏的整体分布，侧边栏的主体和页脚部分待优化

--- src/views/menu/components/SideBar.vue
+++ src/views/menu/components/SideBar.vue
@@ -1,11 +1,24 @@
 <template>
  <!-- 侧边栏 -->
  <aside :class="{ 'sidebar-open': sidebarOpen }" class="sidebar">
-      <div class="sidebar-content-wrap">
+
+  <!-- 顶部固定的关闭和标签 -->
+      <!-- 顶部 -->
+      <header class="sidebar-header">
         <div class="close" @click="toggleSidebar">
-        <img src="@/assets/close.svg" alt="">
+        <img src="@/assets/close.svg" alt="" class="close-img">
+        </div>
+
+        <div class="sidebar-tag">
+          <div class="sidebar-tag-text-wrap">
+            <div>标签1</div>
           </div>
-        <div class="main-content">
+          <div class="sidebar-tag-more">></div>
+        </div>
+      </header>
+
+        <!-- 主要内容： -->
+        <div class="sidebar-main-content">
           <div class="task-wrap shine" >
             <div class="task-tick"></div>
   <!-- <p class="task-text">{{taskValue}}</p> -->
@@ -45,8 +58,11 @@
 
           </div>
 
-        <footer class="create">创建于2024年11月1日</footer>
-  </div>
+          <!-- 底部 -->
+        <footer class="sidebar-footer">
+          <div class="sidebar-tag"># 点击输入</div>
+          <div class="sidebar-create">创建于2024年11月1日</div>
+        </footer>
 
     </aside>
 </template>
@@ -77,34 +93,70 @@ onBeforeUnmount(() => {
 </script>
 
 <style scoped>
-
-/* 布局基础样式
-.app-container {
-  display: flex;
-} */
 /* 侧边栏样式 */
 .sidebar {
-  position: relative;
+
   width: 0;
-  height: 98vh;
-  align-items: center;
+  height: 96vh;
+  min-height: 250px;
+  /* align-items: center; */
   overflow: hidden;
   transition: width 0.3s ease; /* 添加过渡效果 */
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-  .sidebar-content-wrap {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 736px;
+  .sidebar-header,.sidebar-footer {
+    width: 350px;
+    height: 70px;
+  }
+
+  /* 侧边栏头部部分 */
+  .sidebar-header {
+    display: grid;
+    grid-template-rows: 0.9fr 1.1fr;
 
     .close {
-    flex:1;
-    position: absolute;
-    right: 10px;
-    top: 10px;
+      position: relative;
+
+      .close-img {
+        position: absolute;
+        right: 15px;
+        top: 10px;
+      }
     }
-  .main-content {
-    flex:8;
+
+    /* 标签 */
+    .sidebar-tag {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      padding-left: 10px;
+      padding-right: 25px;
+      border-top: 0.5px solid rgba(255, 255, 255, 0.2);
+      border-bottom: 0.5px solid  rgba(255, 255, 255, 0.2);
+
+      /* 包裹的标签文字 */
+      .sidebar-tag-text-wrap {
+
+
+      }
+
+      .sidebar-tag-more {
+        width: 20px;
+        height: 20px;
+
+      }
+
+    }
+    
+  }
+
+  /* 侧边栏主体部分 */
+  .sidebar-main-content {
+    flex:1;
+    overflow-y: auto;
     display: flex;
     flex-direction: column;
     width: 100%;
@@ -113,23 +165,18 @@ onBeforeUnmount(() => {
 
     }
 
-  .create {
-    flex:1;
-    border-top: 1px solid white;
-  }
 
   }
 
 
 
-}
 
 .sidebar-open {
   width: 350px; /* 侧边栏打开时的宽度 */
 }
 
 /* 主内容样式 */
-.main-content {
+.sidebar-main-content {
   display: flex;
   align-items: center;
   flex: 1;
